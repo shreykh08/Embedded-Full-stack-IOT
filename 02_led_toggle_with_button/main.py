a43bdd4bd527efb36b627b70af29427356dc12da
Shrey Khandelwal <125605145+shreykh08@users.noreply.github.com>
2024-05-01 23:07:27 +0530

    Add files via upload

---
+++ 02_led_toggle_with_button/main.py
@@ -0,0 +1,19 @@
+
+# toggle the state of led with each button press 
+
+from machine import Pin
+
+# create object for led pin
+led = Pin(2,Pin.OUT)
+# create object for button pin
+button = Pin(0,Pin.IN)
+
+# run forever 
+while True:
+    if not button.value():
+        led.value(not led.value())
+        print("LED turned on" if led.value() else "LED turned off")
+        # wait till the button is pressed
+        while not button.value():
+            pass
+    
