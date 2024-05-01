3cafc9643df8325460216d3afb092cf7d004c24c
Shrey Khandelwal <125605145+shreykh08@users.noreply.github.com>
2024-05-01 23:00:55 +0530

    Add files via upload

---
+++ 16_Connect_with_WiFi/main.py
@@ -0,0 +1,18 @@
+
+# Connect ESP32 with wifi network
+# Note: before running the code
+# enter your network ssid in place of 'ssid_name'
+# enter your network password in place of 'password'
+
+import network
+
+# object for wlan
+wlan = network.WLAN(network.STA_IF)
+# activate wifi driver
+wlan.active(True)
+
+# connect esp32 with some existing wifi network
+wlan.connect('ssid_name','password')
+# show the connection status
+if wlan.isconnected():
+    print('Connection established')
